40ce4e428930aea03f83d318e0bbacb9f32d6aaf
replityash1 <replityash1@gmail.com>
2026-08-11 12:57:13 +0530

    Update drive.py

--- src/drive.py
+++ src/drive.py
@@ -1,1 +1,48 @@
+import os
+import io
+from google.oauth2.credentials import Credentials
+from googleapiclient.discovery import build
+from googleapiclient.http import MediaIoBaseDownload, MediaFileUpload
 
+def get_drive_service():
+    creds = Credentials(
+        token=None,
+        refresh_token=os.environ["GOOGLE_REFRESH_TOKEN"],
+        client_id=os.environ["GOOGLE_CLIENT_ID"],
+        client_secret=os.environ["GOOGLE_CLIENT_SECRET"],
+        token_uri="https://oauth2.googleapis.com/token",
+    )
+    return build("drive", "v3", credentials=creds)
+
+def list_pdfs(service, folder_id):
+    query = f"'{folder_id}' in parents and mimeType='application/pdf' and trashed=false"
+    results = service.files().list(q=query, fields="files(id, name)").execute()
+    return results.get("files", [])
+
+def download_file(service, file_id, destination_path):
+    request = service.files().get_media(fileId=file_id)
+    fh = io.FileIO(destination_path, "wb")
+    downloader = MediaIoBaseDownload(fh, request)
+    done = False
+    while not done:
+        status, done = downloader.next_chunk()
+
+def upload_file(service, local_path, parent_folder_id, filename=None):
+    file_metadata = {
+        "name": filename or os.path.basename(local_path),
+        "parents": [parent_folder_id],
+    }
+    media = MediaFileUpload(local_path, resumable=True)
+    file = service.files().create(
+        body=file_metadata, media_body=media, fields="id"
+    ).execute()
+    return file.get("id")
+
+def create_drive_folder(service, name, parent_folder_id):
+    file_metadata = {
+        "name": name,
+        "mimeType": "application/vnd.google-apps.folder",
+        "parents": [parent_folder_id],
+    }
+    folder = service.files().create(body=file_metadata, fields="id").execute()
+    return folder.get("id")
